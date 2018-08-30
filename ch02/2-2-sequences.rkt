cfa8f95bbf1341448e0d22da3e2102148551d49a
zehnpaard <zehnpaard@googlemail.com>
2018-08-30 18:32:39 +0900

    Problem 2.33

---
+++ ch02/2-2-sequences.rkt
@@ -0,0 +1,11 @@
+#lang racket
+(define accumulate foldr)
+
+(define (map f seq)
+  (accumulate (lambda (x y) (cons (f x) y)) '() seq))
+
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+(define (length seq)
+  (accumulate (lambda (x y) (+ 1 y)) 0 seq))
