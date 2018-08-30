3534eb8ea5fe203d8abe1b7beba10f5357f226f2
zehnpaard <zehnpaard@googlemail.com>
2018-08-30 19:06:06 +0900

    Problem 2.36

--- ch02/2-2-sequences.rkt
+++ ch02/2-2-sequences.rkt
@@ -25,3 +25,9 @@
                          (count-leaves x)))
                    t)))
 
+;2.36
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      '()
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
