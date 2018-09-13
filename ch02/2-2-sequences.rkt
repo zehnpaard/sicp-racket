af1fe93c13279b3bbe7aa50fd0e13687d5dce551
zehnpaard <zehnpaard@googlemail.com>
2018-09-13 19:32:13 +0900

    Initial function for 8-queens

--- ch02/2-2-sequences.rkt
+++ ch02/2-2-sequences.rkt
@@ -117,3 +117,18 @@
 
 (define (sum-triplets n)
   (filter (lambda (xs) (= n (sum xs))) (unique-triplets n)))
+
+;2.42
+(define (queen board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+      (list empty-board)
+      (filter
+       (lambda (positions) (safe? k positions))
+       (flatmap
+        (lambda (rest-of-queens)
+          (map (lambda (new-row)
+                 (adjoin-position new-row k rest-of-queens))
+               (enumerate-interval 1 board-size)))
+        (queen-cols (- k 1))))))
+  (queen-cols board-size))
