c52c3105040016cad4ed9fe763072d0e8a7c09ac
zehnpaard <zehnpaard@googlemail.com>
2018-09-20 18:52:18 +0900

    Add right-split and up-split

--- ch02/2-2-picture.rkt
+++ ch02/2-2-picture.rkt
@@ -7,3 +7,14 @@
 (define (pfour p)
   (below (ptwo p) (ptwo p)))
 
+(define (right-split p n)
+  (if (zero? n)
+    p
+    (let ((q (right-split p (- n 1))))
+      (beside p (below q q)))))
+
+(define (up-split p n)
+  (if (zero? n)
+    p
+    (let ((q (up-split p (- n 1))))
+      (below p (beside q q)))))
