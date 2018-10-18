8f6262cdbf57d75f145b64d3bdbef5f8844830c4
zehnpaard <zehnpaard@googlemail.com>
2018-10-18 18:34:36 +0900

    2.59

--- ch02/2-2-sets-as-unordered-lists.rkt
+++ ch02/2-2-sets-as-unordered-lists.rkt
@@ -18,3 +18,14 @@
         (else
          (intersection-set (cdr set1) (set2)))))
 
+; 2.59
+(define (union-set set1 set2)
+  (cond ((null? set1)
+         set2)
+        ((null? set2)
+         set1)
+        ((element-of-set? (car set1) set2)
+         (intersection-set (cdr set1) (set2)))
+        (else
+         (cons (car set1)
+               (intersection-set (cdr set1) (set2))))))
