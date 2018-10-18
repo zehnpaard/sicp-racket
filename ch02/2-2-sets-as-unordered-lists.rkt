631382ef0749e8b08abba2ac91639bb55023a4bd
zehnpaard <zehnpaard@googlemail.com>
2018-10-18 18:40:43 +0900

    2.59 bug fix

--- ch02/2-2-sets-as-unordered-lists.rkt
+++ ch02/2-2-sets-as-unordered-lists.rkt
@@ -25,7 +25,7 @@
         ((null? set2)
          set1)
         ((element-of-set? (car set1) set2)
-         (intersection-set (cdr set1) (set2)))
+         (union-set (cdr set1) (set2)))
         (else
          (cons (car set1)
-               (intersection-set (cdr set1) (set2))))))
+               (union-set (cdr set1) (set2))))))
