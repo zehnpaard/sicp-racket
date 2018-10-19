1629024f0b3a2c01b403e87f43a6c91cf65d38a4
zehnpaard <zehnpaard@googlemail.com>
2018-10-19 18:51:30 +0900

    Add discussion on trade-offs between implementations

--- ch02/2-2-sets-as-unordered-lists-non-unique.rkt
+++ ch02/2-2-sets-as-unordered-lists-non-unique.rkt
@@ -30,4 +30,25 @@
 ; where n is the length of the first input (which contains only unique elements)
 ; and m is the length of the second input (which contains only unique elements)
 
-;TODO discuss trade-offs
+; Trade-offs
+; Not deduping the list has positive impact on adjoin-set
+; - adjoin-set: linear -> constant
+; Potentially positive impact on union-set
+; - union-set: n * m -> n'
+; Negative on element-of-set? and intersection-set
+; - element-of-set?: n -> n'
+; - intersection-set: n * m -> n' * m'
+
+; Whether or not union-set is better deduped depends on the number of overlapping
+; elements - if the number is very high, it is possible that deduped n*m is better than
+; undeduped n'.
+
+; Also adjoin-set and union-set can be considered adding data,
+; elements-of-set? and intersection-set can be considered querying data
+; Adding data is (likely) cheaper without deduping, query with deduping
+; If the use-case requires storing a lot of new data and infrequently querying it, then
+; not deduping can make sense
+
+; In summary, though the relationships are somewhat more subtle, broadly speaking
+; raw (rare) <- duplication in data -> frequent) dedupe
+; raw (store) <- balance of actions on data -> (query) dedupe
