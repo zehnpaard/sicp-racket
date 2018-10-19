5ef74a3535946d8249f22fb103e77b0543ec9ad9
zehnpaard <zehnpaard@googlemail.com>
2018-10-19 18:26:44 +0900

    Fix bugs in intersection/union for unordered-list representation

--- ch02/2-2-sets-as-unordered-lists.rkt
+++ ch02/2-2-sets-as-unordered-lists.rkt
@@ -14,9 +14,9 @@
          '())
         ((element-of-set? (car set1) set2)
          (cons (car set1)
-               (intersection-set (cdr set1) (set2))))
+               (intersection-set (cdr set1) set2)))
         (else
-         (intersection-set (cdr set1) (set2)))))
+         (intersection-set (cdr set1) set2))))
 
 ; 2.59
 (define (union-set set1 set2)
@@ -25,10 +25,10 @@
         ((null? set2)
          set1)
         ((element-of-set? (car set1) set2)
-         (union-set (cdr set1) (set2)))
+         (union-set (cdr set1) set2))
         (else
          (cons (car set1)
-               (union-set (cdr set1) (set2))))))
+               (union-set (cdr set1) set2)))))
 
 (define (union-set-b set1 set2)
   (cond ((null? set1)
